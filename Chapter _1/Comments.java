2b57c8c1685fe1fa0d28d465a0a8f9a109372ec6
HeavenJamesPano-UCM <heavenjamesapano@gmail.com>
2025-08-29 21:24:52 +0800

    Add files via upload

---
+++ Chapter _1/Comments.java
@@ -0,0 +1,8 @@
+public class Comments{
+public static void main (String[]args){
+
+    // Line comment
+    /* Block Comment */
+    /** Javadoc Comment */
+    }
+}
---
+++ Chapter _1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong{
+    public static void main (String[]args){
+
+        System.out.println("You hold me like I'm the one who's precious\n" +
+                            "I hate to break it to you, but it's just the other way around\n" +
+                            "You can thank your stars all you want\n" + 
+                            "But I'll always be the lucky one");
+    }
+}
